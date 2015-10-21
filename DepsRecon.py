e8b06a3d7c7a1cad43cea56a9b7e3aa6af0f6695
Eric W. Tramel <eric.tramel@gmail.com>
2015-10-21 10:56:56 +0200

    More Notebook Updates

--- DepsRecon.py
+++ DepsRecon.py
@@ -34,6 +34,52 @@ def RandomKSparseSignal(K,N):
     
     return x, s
 
+def KSparseApproximation(x,K):
+    """ Retain the top K coefficients in  x.
+    
+    Given a signal, find the top K-magnitude coefficients.
+    Return a signal which retains these identified coefficients
+    and sets the rest to 0.
+    
+    Input
+    ---
+    x: array_like, float
+       The signal that we wish to approximate
+       
+    K: int
+       Number of coefficients to retain.
+    
+    Returns
+    ---
+    xT: array_like, float
+        The K-Sparse approximation of x
+    S:  array_like, int
+        An array of the support index locations in x.    
+    """
+    S = np.argsort(abs(x),axis=0)[::-1][:K]
+    xT = np.zeros_like(x)
+    xT[S] = x[S]
+    
+    return xT, S
+
+def RandomLaplaceSignal(scale,N):
+    """ Random realization of an iid Laplace signal. 
+    
+        Input
+        ---
+        K: float
+           Scale parameter for the Laplacian distribution.
+        N: int
+           Signal dimensionality (length)
+        
+        Returns
+        ---
+        x: array_like, float
+           The iid Laplace signal realization, shaped ``(n,1)``                
+    """
+    x = np.random.laplace(0.0,scale=scale,size=(N,1))    
+    return x
+
 def ShowSignal(x):
     plt.rc('text',usetex=True)
     plt.rc('font', family='serif')
@@ -73,14 +119,30 @@ def ShowRecovery(x,y):
     
     f = plt.figure(figsize=(10,5),dpi=300)
     plt.plot(x,':ob');
-    plt.plot(y,':xr');
+    plt.plot(y,'-xr');
     plt.axis("tight")
     plt.grid(True)
     plt.xlabel("Index", fontsize=16)
     plt.ylabel("Value", fontsize=16)
     
-    mseval = MSE(x,y)
+    mseval = MeanSquareError(x,y)
 
-def MSE(x,y):
+def MeanSquareError(x,y):
     mseval = np.mean(np.power(x-y,2))    
     return mseval
+
+def ShowHistory(mseval,resid):
+    plt.rc('text',usetex=True)
+    plt.rc('font', family='serif')
+    
+    f = plt.figure(figsize=(10,5),dpi=300)
+    plt.plot(mseval,'-b',linewidth=2,label="$\\frac{1}{N}||x - x^{(t)}||_2^2$");
+    plt.plot(resid,'-r',linewidth=2,label="$\\frac{1}{M}||y - F x^{(t)}||_2^2$");
+    plt.yscale('log')
+    plt.ylim((1e-10,1e1));
+    plt.xlim((0,np.size(mseval)-1))
+    plt.grid(True)
+    plt.xlabel("Iteration", fontsize=16)
+    plt.ylabel("Value", fontsize=16)
+    plt.title("Evolution of Reconstruction")
+    plt.legend()
